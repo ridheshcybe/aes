0ae44af58da183313a2a5f9f383fe32c7a9a5daf
ridheshcybe <80441770+ridheshcybe@users.noreply.github.com>
2024-02-09 11:31:51 +0000

    Update encryption and decryption functions to use OpenSSL library. Add hashing function using Web Crypto API.

--- index.c
+++ index.c
@@ -1,22 +1,81 @@
-/*
- * Copyright 2024 Cybe & ridhesh walavalkar.  All rights reserved.
- */
-#include <stdio.h>
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <string.h>
 #include <emscripten.h>
 
-int main()
-{
-    return 0;
-};
-
+/**
+ * encrypt function
+ * give plaintext length & data
+ * give string key
+ * give string iv
+*/
 EMSCRIPTEN_KEEPALIVE
-char *encrypt(char *key, char *text)
+char *encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv)
 {
-    return "ij";
+    EVP_CIPHER_CTX *ctx;
+
+    int len;
+    char *ciphertext;
+    int ciphertext_len;
+
+    if (!(ctx = EVP_CIPHER_CTX_new()))
+        return "ENCRYPTTION_CIPHER_CTX_CREATE_ERROR";
+
+    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+        return "ENCRYPTION_INIT_FAILURE";
+
+    if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+        return "ENCRYPTION_UPDATE_FAILURE";
+    ciphertext_len = len;
+
+    if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
+        return "ENCRYPTION_FINAL_FAILURE";
+    ciphertext_len += len;
+
+    EVP_CIPHER_CTX_free(ctx);
+
+    char *output = ciphertext_len + '()' + ciphertext;
+
+    return output;
 }
 
+/**
+ * decrypt function
+ * give ciphertext length & data
+ * give string key
+ * give string iv
+*/
 EMSCRIPTEN_KEEPALIVE
-char *decrypt(char *key, char *text)
+char *decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *iv)
 {
-    return "hi";
+    EVP_CIPHER_CTX *ctx;
+
+    int len;
+    int plaintext_len;
+    char *plaintext;
+
+    if (!(ctx = EVP_CIPHER_CTX_new()))
+        return "DECRYPTION_CIPHER_CTX_CREATE_ERROR";
+
+    if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+        return "DECRYPTION_INIT_FAILURE";
+
+    if (1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+        return "DECRYPTION_UPDATE_FAILURE";
+    plaintext_len = len;
+
+    if (1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
+        return "DECRYPTION_FINAL_FALIURE";
+    plaintext_len += len;
+
+    EVP_CIPHER_CTX_free(ctx);
+
+    char *output = plaintext_len + '()' + plaintext;
+
+    return output;
+}
+
+int main(){
+    return 0;
 }
--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const pwdhtm = document.getElementById("pwd");
 const typehtm = document.getElementById("type");
 const inputhtm = document.getElementById("in");
 const outhtm = document.getElementById("out");
-function run(ev) {
-    const pwd = pwdhtm.innerText;
+
+async function digestMessage(message) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(message);
+    const hash = await crypto.subtle.digest("SHA-256", data);
+    return hash;
+  }
+
+async function run(ev) {
+    const pwd = await digestMessage(pwdhtm.innerText);
     const input = inputhtm.innerText;
     const type = typehtm.value == "enc" ? "encrypt" : "decrypt";
-outhtm.innerHTML = Module.ccall(type,"number", ["string","string"], pwd,input)
+
+    outhtm.innerHTML = Module.ccall(type,"number", ["string","number","string"], input,input.length,pwd,crypto.randomUUID())
 }
